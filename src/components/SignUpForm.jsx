36f70ebe3138109b1e8624a880239f4ed4c7b23a
Sourabh_kakani <kakanisourabh23@gmail.com>
2024-08-26 13:40:41 +0100

    firebase error pdated

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,26 +1,53 @@
 import React from "react";
 import "../App.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignUpForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log("ACCOUNT CREATED");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="signup-container">
-      <form action="" className="signup-form">
+      <form action="" className="signup-form" onSubmit={handleSubmit}>
         <h2>Sign Up</h2>
 
         <label htmlFor="email">
           Email:
-          <input type="text" name="email" id="" />
+          <input
+            type="text"
+            name="email"
+            id=""
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
 
         <label htmlFor="password">
           Password:
-          <input type="password" name="password" id="" />
+          <input
+            type="password"
+            name="password"
+            id=""
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
 
-        <button>Sign Up</button>
+        <button type="submit">Sign Up</button>
 
         <p>
-          Already Registered? <a href="#">Login</a>
+          Already Registered? <Link to="/login">Login</Link>
         </p>
       </form>
     </div>
